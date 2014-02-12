5c087969b98404cc34127c0607f7961c1d10cd78
Leif Johnson <leif@leifjohnson.net>
2014-02-11 21:06:07 -0600

    Add a skeleton config for documentation.

---
+++ docs/conf.py
@@ -0,0 +1,94 @@
+import os
+import sys
+
+import sphinx_rtd_theme
+
+#needs_sphinx = '1.0'
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    #'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    #'sphinx.ext.pngmath',
+    'sphinx.ext.viewcode',
+    'numpydoc',
+    ]
+autosummary_generate = True
+autodoc_default_flags = ['members', 'inherited-members']
+templates_path = ['_templates']
+source_suffix = '.rst'
+source_encoding = 'utf-8-sig'
+master_doc = 'index'
+project = u'c3d'
+copyright = u'2013, Leif Johnson'
+version = '0.1.0'
+release = '0.1.0'
+#language = None
+#today = ''
+#today_fmt = '%B %d, %Y'
+exclude_patterns = ['_build']
+#default_role = None
+#add_function_parentheses = True
+#add_module_names = True
+#show_authors = False
+pygments_style = 'sphinx'
+#modindex_common_prefix = []
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ['_static']
+#html_title = None
+#html_short_title = None
+#html_logo = None
+#html_favicon = None
+#html_last_updated_fmt = '%b %d, %Y'
+#html_use_smartypants = True
+#html_sidebars = {}
+#html_additional_pages = {}
+#html_domain_indices = True
+#html_use_index = True
+#html_split_index = False
+#html_show_sourcelink = True
+#html_show_sphinx = True
+#html_show_copyright = True
+#html_use_opensearch = ''
+#html_file_suffix = None
+htmlhelp_basename = 'c3ddoc'
+
+latex_elements = {
+#'papersize': 'letterpaper',
+#'pointsize': '10pt',
+#'preamble': '',
+}
+latex_documents = [
+  ('index', 'c3d.tex', u'c3d Documentation',
+   u'Leif Johnson', 'manual'),
+]
+#latex_logo = None
+#latex_use_parts = False
+#latex_show_pagerefs = False
+#latex_show_urls = False
+#latex_appendices = []
+#latex_domain_indices = True
+
+man_pages = [
+    ('index', 'c3d', u'c3d Documentation',
+     [u'Leif Johnson'], 1)
+]
+#man_show_urls = False
+
+texinfo_documents = [
+  ('index',
+   'c3d',
+   u'c3d Documentation',
+   u'Leif Johnson',
+   'c3d',
+   'One line description of project.',
+   'Miscellaneous'),
+]
+#texinfo_appendices = []
+#texinfo_domain_indices = True
+#texinfo_show_urls = 'footnote'
+
+intersphinx_mapping = {'http://docs.python.org/': None}
---
+++ docs/index.rst
@@ -0,0 +1,16 @@
+C3D
+===
+
+Quickstart
+----------
+
+:class:`c3d.Reader`
+
+.. autosummary::
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
