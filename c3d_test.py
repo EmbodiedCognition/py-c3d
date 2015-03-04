0431f142a1ababa865c3c9021adf834184071c89
Leif Johnson <leif@lmjohns3.com>
2015-03-04 17:13:15 -0600

    + skeleton test file

---
+++ c3d_test.py
@@ -0,0 +1,14 @@
+import c3d
+import unittest
+
+
+class ReaderTest(unittest.TestCase):
+    pass
+
+
+class WriterTest(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
