5fd638d921402df920b44dacaa4cde516e217d2f
Leif Johnson <leif@lmjohns3.com>
2015-03-04 21:53:56 -0600

    Get reader tests working!

--- test/c3d_test.py
+++ test/c3d_test.py
@@ -18,8 +18,7 @@ ZIPS = (
     ('https://www.c3d.org/data/sample08.zip', 'params.zip'),
 )
 
-
-class ReaderTest(unittest.TestCase):
+class Base(unittest.TestCase):
     def setUp(self):
         if not os.path.isdir(TEMP):
             os.makedirs(TEMP)
@@ -40,31 +39,72 @@ class ReaderTest(unittest.TestCase):
         with zipfile.ZipFile(os.path.join(TEMP, zf)) as z:
             return io.BytesIO(z.open(fn).read())
 
-    def test_format(self):
-        for fn in self._c3ds('formats.zip'):
-            try:
-                r = c3d.Reader(self._get('formats.zip', fn))
-            except Exception as e:
-                logging.exception('formats.zip:{}'.format(fn.filename))
-                assert False
-                continue
-            print(r.header)
-            assert r.points_per_frame() == 12
 
-    def test_params(self):
-        for fn in self._c3ds('params.zip'):
-            try:
-                r = c3d.Reader(self._get('params.zip', fn))
-            except Exception as e:
-                logging.exception('params.zip:{}'.format(fn.filename))
-                assert False
-                continue
-            print(r)
-            assert r.points_per_frame() == 12
+class ReaderTest(Base):
+    def test_format_pi(self):
+        r = c3d.Reader(self._get('formats.zip', 'Eb015pi.c3d'))
+        print(r.header)
+        assert r.points_per_frame() == 26
+        assert r.frame_rate() == 50
+
+    def test_format_pr(self):
+        r = c3d.Reader(self._get('formats.zip', 'Eb015pr.c3d'))
+        print(r.header)
+        assert r.points_per_frame() == 26
+        assert r.frame_rate() == 50
+
+    def test_paramsa(self):
+        r = c3d.Reader(self._get('params.zip', 'TESTAPI.c3d'))
+        print(r.header)
+        assert r.points_per_frame() == 26
+        assert r.frame_rate() == 50
+
+    def test_paramsb(self):
+        r = c3d.Reader(self._get('params.zip', 'TESTBPI.c3d'))
+        print(r.header)
+        for g in r._groups.values():
+            for p in g._params.values():
+                if len(p.dimensions) == 0:
+                    val = None
+                    width = len(p.bytes)
+                    if width == 2:
+                        val = p.int16_value
+                    elif width == 4:
+                        val = p.float_value
+                    else:
+                        val = p.int8_value
+                    print('{0.name}.{1.name} = {2}'.format(g, p, val))
+        assert r.points_per_frame() == 26
+        assert r.frame_rate() == 50
+        assert r.analog_per_frame() == 16
+        assert r.get_float('POINT:RATE') == 50
+        assert r.get_float('ANALOG:RATE') == 200
+
+    def test_paramsc(self):
+        r = c3d.Reader(self._get('params.zip', 'TESTCPI.c3d'))
+        print(r.header)
+        assert r.points_per_frame() == 26
+        assert r.frame_rate() == 50
+
+    def test_paramsd(self):
+        r = c3d.Reader(self._get('params.zip', 'TESTDPI.c3d'))
+        print(r.header)
+        assert r.points_per_frame() == 26
+        assert r.frame_rate() == 50
 
 
-class WriterTest(unittest.TestCase):
-    pass
+class WriterTest(Base):
+    def test_paramsd(self):
+        r = c3d.Reader(self._get('params.zip', 'TESTDPI.c3d'))
+        w = c3d.Writer(
+            point_frame_rate=r.frame_rate(),
+            analog_frame_rate=r.analog_frame_rate(),
+            point_scale_factor=r.scale_factor(),
+            gen_scale=r.get_float('ANALOG:GEN_SCALE'),
+        )
+        w.add_frames((p, a) for _, p, a in r.read_frames())
+        h = io.BytesIO()
+        w.write(h)
 
 
 if __name__ == '__main__':
