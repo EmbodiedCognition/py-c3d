d723ed4652bf468a63410ef787d77a781c9058ed
LITTLEONE\Mattias <mattias-fredriksson91@hotmail.se>
2020-06-17 17:47:59 +0200

    Header binary format comment

--- c3d.py
+++ c3d.py
@@ -90,6 +90,7 @@ class Header(object):
 		POINTS:ACTUAL_END_FIELD parameters.
 	'''
 
+    # Read/Write header formats, reads values as unsigned int's for conversion.
 	BINARY_FORMAT_WRITE = '<BBHHHHHfHHf270sHH214s'
 	BINARY_FORMAT_READ = '<BBHHHHHIHHI270sHH214s'
 
