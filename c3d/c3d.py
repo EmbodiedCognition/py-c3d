a8b353bd87c240debfcb4170648542e6d4d9e4d3
MattiasF <mattias-fredriksson91@hotmail.se>
2021-06-28 17:39:56 +0200

    Added tests: ManagerGroupTests.test_Manager_add_group ManagerGroupTests.test_Manager_remove_group_from_numeric ManagerGroupTests.test_Manager_remove_group_from_name

--- c3d/c3d.py
+++ c3d/c3d.py
@@ -1082,7 +1082,7 @@ class Manager(object):
         '''
         if not is_integer(group_id):
             raise ValueError('Expected Group numerical key to be integer, was %s.' % type(group_id))
-        if not isinstance(name, str):
+        if not (isinstance(name, str) or name is None):
             raise ValueError('Expected Group name key to be string, was %s.' % type(name))
         group_id = int(group_id) # Assert python int
         if group_id in self._groups:
